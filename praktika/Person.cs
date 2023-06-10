dac6c84566642557d9f13d1eec48b7efdd20d4db
Irinaa09 <102945084+Irinaa09@users.noreply.github.com>
2023-06-10 23:09:30 +0300

    Add files via upload

---
+++ praktika/Person.cs
@@ -0,0 +1,25 @@
+﻿
+namespace praktika;
+
+public class Person
+{
+    private string _name;
+    public int Id
+    {
+        // getter - получаеь значение свойства
+        get;
+        // setter - не получает
+        set;
+    }
+    public Person(int id, string name)
+    {
+        Id = id;
+        _name = name;
+    }
+
+    //method
+    public string GetName()
+    {
+        return _name;
+    }
+}
---
+++ praktika/Program.cs
@@ -0,0 +1,5 @@
+﻿
+using praktika;
+
+Person personWithName = new Person( 1405, "Ira" );
+Console.WriteLine(personWithName.Id);
